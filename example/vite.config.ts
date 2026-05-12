c63928f7e5b65fbf21f4f6b014812a45c35137e1
Junjay Tan <junjay@jibelabs.com>
2026-05-12 14:24:03 -0400

    Revert example into a pure vite react app
    Temporarily move the workshop related files that were there into the main src dir

--- example/vite.config.ts
+++ example/vite.config.ts
@@ -1,15 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { workshopPlugin } from '../src/plugin.ts'
 
 export default defineConfig({
-  plugins: [react(), workshopPlugin({ include: 'src/**/*.test.{ts,tsx}' })],
-  build: {
-    rollupOptions: {
-      input: {
-        main: 'index.html',
-        frame: 'frame.html',
-      },
-    },
-  },
+  plugins: [react()]
 })
